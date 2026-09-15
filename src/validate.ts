c88629eb9a404386f703fcf4432905b1a58b1ada
Ian Copp <copp.ian@gmail.com>
2026-09-15 11:42:47 -0700

    feat!: add type assertion to `validate`
    The type assertion automatically narrows down the implicit typing of
    whatever value it is given to the templated string type.
    
    This is a breaking change because depending on strictness, this may
    make existing code error if it's doing something invalid after the
    `validate` check.

--- src/validate.ts
+++ src/validate.ts
@@ -1,6 +1,6 @@
 import REGEX from './regex.js';
 
-function validate(uuid: unknown) {
+function validate(uuid: unknown): uuid is string {
   return typeof uuid === 'string' && REGEX.test(uuid);
 }
 
